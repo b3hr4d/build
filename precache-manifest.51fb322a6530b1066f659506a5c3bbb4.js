909cf60b129ef06eaca60586b7c582a1f22d4e09
b3hr4d <behradmusiclove@yahoo.com>
2020-08-23 15:01:22 +0430

    0.2

---
+++ precache-manifest.51fb322a6530b1066f659506a5c3bbb4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b208e0ca0bbeb456e0b53a40c7596f4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41676f8af1514ef768f2",
+    "url": "/static/css/main.1782e053.chunk.css"
+  },
+  {
+    "revision": "9c6cfde1ea37ed3194ff",
+    "url": "/static/js/2.680eda51.chunk.js"
+  },
+  {
+    "revision": "ae219b0ea28bee1eff6f466251b6746e",
+    "url": "/static/js/2.680eda51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41676f8af1514ef768f2",
+    "url": "/static/js/main.eaab6a57.chunk.js"
+  },
+  {
+    "revision": "d2f2b64a5fd50e5b0486",
+    "url": "/static/js/runtime-main.2ae528a0.js"
+  },
+  {
+    "revision": "48cf9b9c113e0bc5c9f6563b79230c6f",
+    "url": "/static/media/Black-Hole.48cf9b9c.jpg"
+  },
+  {
+    "revision": "b495bc14c98fd308bafefcfd31fddd20",
+    "url": "/static/media/Vazir-Black.b495bc14.woff2"
+  },
+  {
+    "revision": "ed4a58c261df0539ab08fc9064b20e33",
+    "url": "/static/media/Vazir-Light.ed4a58c2.woff2"
+  },
+  {
+    "revision": "4bd4185d80a8f3bc85f1995457ea1e42",
+    "url": "/static/media/Vazir-Medium.4bd4185d.woff2"
+  },
+  {
+    "revision": "dfc05881920f7bc8593ca145a79407dd",
+    "url": "/static/media/Vazir-Thin.dfc05881.woff2"
+  },
+  {
+    "revision": "95ee69c4226f615b56f51548f5c932b1",
+    "url": "/static/media/Vazir.95ee69c4.woff2"
+  }
+]);
